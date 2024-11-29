9f08016b75cf6838edabbf8f4c695c9b1d754d28
BlendMW <64715707+BlendMW@users.noreply.github.com>
2024-11-29 13:31:32 +0300

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,23 +1,15 @@
+import { defineBackend } from "@aws-amplify/backend";
+
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage, visaTemplates, manualVisaDocuments } from './storage/resource';
 import { functions } from './functions/resource';
 
-export default ({
+export default defineBackend({
   auth,
   data,
   storage,
   visaTemplates,
   manualVisaDocuments,
   functions,
-  getInstance() {
-    return {
-      auth,
-      data,
-      storage,
-      functions,
-      visaTemplates,
-      manualVisaDocuments,
-    };
-  },
 });
